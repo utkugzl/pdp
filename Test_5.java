4b442c705b4b76f228e71fdd62c1cecd8a4b94b2
utkugzl <utku.guzel44@gmail.com>
2024-03-08 18:13:56 +0300

    test 5 added

---
+++ Test_5.java
@@ -0,0 +1,17 @@
+package odev;
+import java.util.UUID;
+public class Test_3 {
+    // calistir
+    private String motorNo;
+    private boolean calisiyor;
+    public void calistir() {
+    calisiyor = true;
+    }
+    /**
+    * Motorun durdurulması //
+    */
+    public void durdur() {
+    calisiyor = false;
+    }
+    // motor no getir
+}
